402fcd2418baad3d7c32999068da0cdf483d16fd
Tracy Chen <leidan@uchicago.edu>
2023-10-26 14:47:25 -0500

    test deepspeed config

--- train.py
+++ train.py
@@ -94,7 +94,6 @@ deepspeed_config = {
             "lr": learning_rate,
             "weight_decay": weight_decay,
             "betas": [beta1, beta2],
-            "grad_clip": grad_clip,
             "decay_lr": decay_lr,
             "warmup_iters": warmup_iters,
             "lr_decay_iters": lr_decay_iters,
