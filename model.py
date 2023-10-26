4beb1924c254b01b324bcba6e4c8785f13b2b00a
Tracy Chen <leidan@uchicago.edu>
2023-10-26 12:04:25 -0500

    fix activation checkpoint

--- model.py
+++ model.py
@@ -116,7 +116,7 @@ class GPTConfig:
     n_embd: int = 768
     dropout: float = 0.0
     bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
-    activation_checkpoint = bool = False
+    activation_checkpoint: bool = False
 
 class GPT(nn.Module):
 
